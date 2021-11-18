f44662a12383991bc0505869b227f4d98a71ddc9
Andreas Brandmaier <brandmaier@mpib-berlin.mpg.de>
2021-11-18 15:13:01 +0100

    extended chisq

--- R/chisq.R
+++ R/chisq.R
@@ -16,48 +16,93 @@ generate_chisq <- function(labels.a=c("Antidepressivum","Placebo"), labels.b=c("
   total <- csums[length(csums)]
   df <- rbind(df, csums)
   rownames(df)[nrow(df)] <- ""  
-  colnames(df)[ncol(df)] <- ""
+  names(df)[ncol(df)] <- ""
   
   df_sums <- data.frame(df)
   
-  
+  n21<-df_raw[2,1] 
+  n12<-df_raw[1,2]
   
   for (i in 1:nrows) {
     for (j in 1:ncols) {
       df[i,j] <- rsums[i]*csums[j]/total
     }
   }
-  
+
   
   df_indep <- data.frame(df)
   
-  list(labels.a=labels.a, labels.b=labels.b,tbl=tbl,df_raw=df_raw, df_sums=df_sums, df_indep=df_indep, nrows=nrows, ncols=ncols,alpha=alpha)
+  nij <- simplify(df_raw)
+  eij <- simplify(df_indep[1:nrows,1:ncols])
+  
+  chisq <- round(sum((nij-eij)^2/eij),2)
+  
+  list(labels.a=labels.a, labels.b=labels.b,tbl=tbl,df_raw=df_raw, 
+       df_sums=df_sums, df_indep=df_indep, nrows=nrows, ncols=ncols,alpha=alpha,
+       n12=n12,n21=n21, nij=nij, eij=eij, chisq=chisq)
 }
 
-chisq_data_table <- function(x, with_sums=FALSE, expected=FALSE) {
+chisq_data_table <- function(x, type="",caption="") {
   
-  if (with_sums) {
-    if (expected) {
-      knitr::kable(x$df_indep)   
-    } else {
-      knitr::kable( x$df_raw )
-    }
+  # if (with_sums) {
+  #   if (expected) {
+  #     knitr::kable(x$df_indep, caption=caption)   
+  #   } else {
+  #     knitr::kable( x$df_raw, caption=caption )
+  #   }
+  # } else {
+  #   if (expected) {
+  #     knitr::kable(x$df_indep, caption=caption)   
+  #   } else {
+  #     knitr::kable( x$df, caption=caption )
+  #   }
+  # }
+  if (type=="") {
+    if (caption=="") caption="Kontingenztabelle"
+    knitr::kable( x$df_raw, caption=caption )
+  } else if (type=="indep") {
+    if (caption=="") caption="Kontingenztabelle unter Unabhängigkeitsannahme"
+    knitr::kable(x$df_indep, caption=caption)   
+  } else if (type=="sums") {
+    if (caption=="") caption="Kontingenztabelle mit Randsummen"
+    knitr::kable( x$df_sums, caption=caption )
   } else {
-    if (expected) {
-      knitr::kable(x$df_indep)   
-    } else {
-      knitr::kable( x$df_sums )
-    }
+    print("")
   }
   
 }
 
+mcnemar_solution <- function(x) {
+
+ n12 <- x$n12
+ n21 <- x$n21
+ chi2 = round( (n21-n12)^2/(n12+n21), 2)
+ 
+ 
+ paste0("\\chi^2=",
+        
+          "\\frac{(",n12,"-",n21,")^2}{",n12,"+",n21,"}"
+        ,
+        "=",chi2,""
+ )
+}
+
+mcnemar_effectsize <- function(x) {
+  chance <- round(x$n12/x$n21,2)
+  frac <- round((x$n12/(x$n12+x$n21)),2)*100
+  paste0(
+   paste0(" \\frac{",x$n12,"}{",x$n21,"}=", chance),
+   paste0(" \\frac{",x$n12,"}{",(x$n12+x$n21),"}=",frac,"\\%"),
+   "\\\\"
+  )
+}
+
 chisq_solution <- function(x) {
   
-  nij <- simplify(x$df_raw)
-  eij <- simplify(x$df_indep[1:x$nrows,1:x$ncols])
+  nij <- x$nij
+  eij <- x$eij
   
-  chisq <- round(sum((nij-eij)^2/eij),2)
+  chisq <- x$chisq
   
   paste0("\\chi^2=",
          paste0(
@@ -72,27 +117,49 @@ chisq_solution <- function(x) {
 chisq_crit <- function(x) {
   df <- (x$ncols-1)*(x$nrows-1)
   chisqcrt <- round(qchisq(1-x$alpha, df),2)
-  paste0("Der kritische Wert einer $\\chi^2$-Verteilung mit ",df," Freiheitsgrad(en) ist ",chisqcrt)
+  cmp <- x$chi2 >= chisqcrt
+  
+  paste0("Der kritische Wert einer $\\chi^2$-Verteilung mit ",df," Freiheitsgrad(en) ist ",chisqcrt,".")
+  
+}
+
+chisq_decision <- function(x) {
+  df <- (x$ncols-1)*(x$nrows-1)
+  chisqcrt <- round(qchisq(1-x$alpha, df),2)
+  cmp <- x$chi2 >= chisqcrt
+  
+  paste0("Der empirische Wert der Prüfgröße ist ",
+         ifelse(cmp,"größer/gleich","kleiner")
+         ," als der kritische Wert. ", ifelse(cmp,"Wir lehnen die Nullhypothese ab.",
+                                              "Wir können die Nullhypothese nicht ablehnen."))
 }
 
 # ---- # ----
 
-generate_chisq_uni <- function(obs, exp, labels) {
+generate_chisq_uni <- function(obs, exp_relative, labels) {
   
+  n <- sum(obs)
+  exp <- exp_relative*n/100
   terms <- round( (obs-exp)^2/exp, 2)
   
   chi2 <- round(sum(terms),2)
   
   df <- length(obs)-1
   
-  n <- sum(obs)
+ 
   
-  list(obs=obs, exp=exp, terms=terms, chi2=chi2, df=df,n=n, labels=labels)
+  list(obs=obs, exp=exp, terms=terms, chi2=chi2, df=df,n=n, 
+       labels=labels,exp_relative=exp_relative, chi2=chi2)
 }
 
 solution_chisq_uni <- function(x)
 {
-  paste0("\\chi^2=",
+  part1 <- paste(
+    " e_{",x$label,"}=",x$exp_relative,"\\cdot",x$n,
+    collapse="\\\\ "
+  )
+  
+  part2 <- paste0("\\chi^2=",
          paste0(
            
            "\\frac{(",x$obs,"-",x$exp,")^2}{",x$exp,"}", collapse="+"
@@ -100,9 +167,17 @@ solution_chisq_uni <- function(x)
          ),"=",x$chisq
   )
   
+  paste( "\\begin{align}",part1,"\\end{align}","\\\\", part2, collapse=" \\n ")
+  
+}
+
+chisq_effectsize <- function(x) {
+  omega <- round( sqrt(x$chi2/x$n) , 2)
+  
+  paste0("\\omega=\\sqrt{\\frac{",x$chi2,"}{",x$n,"}}=",omega)
 }
 
 chisq_uni_data_table <- function(x)
 {
-  knitr::kable(x$obs)
+  knitr::kable(t(x$obs),col.names = x$labels)
 }
--- R/regression.R
+++ R/regression.R
@@ -4,18 +4,18 @@ generate_regression <- function(n, ryx1, ryx2, rx12, xmu, ymu, sx1,sx2,sy, round
   b1 = round( (ryx1 - ryx2*rx12 ) / (1-rx12^2)* sy/sx1, 2)
   b2 = round( (ryx2 - ryx1*rx12) / (1-rx12^2) * sy/sx2, 2)
   
-  list(n=n, r01=r01, xmu=xmu, ymu=ymu, sx=sx, sy=sy, b1=b1, b2=b2,
+  list(n=n, xmu=xmu, ymu=ymu, sx1=sx1, sx2=sx2, sy=sy, b1=b1, b2=b2,
        ryx1=ryx1, ryx2=ryx2, rx12=rx12) 
 }
 
 solution_coeff <- function(x) {
   paste(
- paste0("b_1=\frac{",x$ryx1,"-",x$ryx2,"\cdot",x$rx12,"}{1-",x$rx12,"^2} \cdot \frac{",x$sy,"}{",x$sx1,"}")  ,
-
-paste0("b_1=\frac{",x$ryx1,"-",x$ryx2,"\cdot",x$rx12,"}{1-",x$rx12,"^2} \cdot \frac{",x$sy,"}{",x$sx1,"}")  
+ paste0("b_1=\\frac{",x$ryx1,"-",x$ryx2,"\\cdot",x$rx12,"}{1-",x$rx12,"^2} \\cdot \\frac{",x$sy,"}{",x$sx1,"}","=",x$b0)  ,
+"\n\n",
+paste0("b_1=\\frac{",x$ryx1,"-",x$ryx2,"\\cdot",x$rx12,"}{1-",x$rx12,"^2} \\cdot \\frac{",x$sy,"}{",x$sx1,"}","=",x$b0)  
   )
 }
 
 solution_std_from_r2 <- function(x) {
-  
+  paste0("s_E=")
 }
