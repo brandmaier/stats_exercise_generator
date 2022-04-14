2402cec6842c3be18281927ad9da4b77646fbbcc
Andreas Brandmaier <andy@brandmaier.de>
2022-04-14 09:29:43 +0200

    renaming

--- render.R
+++ render.R
@@ -1,42 +1,44 @@
 options(encoding = 'UTF-8')
 
+output_path = "publish/"
+
 #
 # render all work sheets
 #
 rmarkdown::render(input="AB1-Varianzanalyse-einfaktoriell.Rmd", 
-                  output_file="AB1-Varianzanalyse-einfaktoriell.pdf", 
+                  output_file=paste0(output_path,"AB1-Varianzanalyse-einfaktoriell.pdf"), 
                   params=list(include_solution=FALSE))
 rmarkdown::render(input="AB1-Varianzanalyse-einfaktoriell.Rmd", 
-                  output_file="AB1-Varianzanalyse-einfaktoriell-mit_Lösung.pdf", 
+                  output_file=paste0(output_path,"AB1-Varianzanalyse-einfaktoriell-mit_Lösung.pdf"), 
                   params=list(include_solution=TRUE))
 
 
 rmarkdown::render(input="AB2-Kontraste.Rmd", 
-                  output_file="AB2-Kontraste.pdf", 
+                  output_file=paste0(output_path,"AB2-Kontraste.pdf"), 
                   params=list(include_solution=FALSE))
 rmarkdown::render(input="AB2-Kontraste.Rmd", 
-                  output_file="AB2-Kontraste-mit-Lösung.pdf", 
+                  output_file=paste0(output_path,"AB2-Kontraste-mit-Lösung.pdf"), 
                   params=list(include_solution=TRUE))
 
-rmarkdown::render(input="AB3-ChiQuadrat.Rmd", 
-                  output_file="AB3-ChiQuadrat.pdf", 
+rmarkdown::render(input="AB-ChiQuadrat.Rmd", 
+                  output_file=paste0(output_path,"AB-ChiQuadrat.pdf"), 
                   params=list(include_solution=FALSE))
-rmarkdown::render(input="AB3-ChiQuadrat.Rmd", 
-                  output_file="AB3-ChiQuadrat-mit-Lösung.pdf", 
+rmarkdown::render(input="AB-ChiQuadrat.Rmd", 
+                  output_file=paste0(output_path,"AB-ChiQuadrat-mit-Lösung.pdf"), 
                   params=list(include_solution=TRUE))
 
 
 rmarkdown::render(input="AB4-Hauptkomponentenanalyse.Rmd", 
-                  output_file="AB4-Hauptkomponentenanalyse.pdf", 
+                  output_file=paste0(output_path,"AB4-Hauptkomponentenanalyse.pdf"), 
                   params=list(include_solution=FALSE))
 rmarkdown::render(input="AB4-Hauptkomponentenanalyse.Rmd", 
-                  output_file="AB4-Hauptkomponentenanalyse-mit-Lösung.pdf", 
+                  output_file=paste0(output_path,"AB4-Hauptkomponentenanalyse-mit-Lösung.pdf"), 
                   params=list(include_solution=TRUE))
 
-rmarkdown::render(input="AB6-Korrelation.Rmd", 
-                  output_file="AB6-Korrelation.pdf", 
+rmarkdown::render(input="AB-Korrelation.Rmd", 
+                  output_file=paste0(output_path,"AB-Korrelation.pdf"), 
                   params=list(include_solution=FALSE))
-rmarkdown::render(input="AB6-Korrelation.Rmd", 
-                  output_file="AB6-Korrelation-mit-Lösung.pdf", 
+rmarkdown::render(input="AB-Korrelation.Rmd", 
+                  output_file=paste0(output_path,"AB-Korrelation-mit-Lösung.pdf"), 
                   params=list(include_solution=TRUE))
 
