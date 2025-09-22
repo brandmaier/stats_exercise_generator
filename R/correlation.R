1b985c47f0e73207f22a46c2f4221a745c3cf44f
brandmaier <brandmaier@mpib-berlin.mpg.de>
2025-09-22 16:38:04 +0200

    fixed errors

--- R/correlation.R
+++ R/correlation.R
@@ -3,15 +3,18 @@ sample_corr <- function(n, r){
                 mu=c(0,0), 
                 Sigma=matrix(c(1,r,r,1),nrow=2))
   
-  }
+}
 
 generate_corr_twogroup <- function(n1, r1, n2, r2, alpha=0.05) {
   
   dat1 <- sample_corr(n1, r1)
   dat2 <- sample_corr(n2, r2)
   
-  test1 = cor.test(d1[,1],d1[,2])
-  test2 = cor.test(d2[,1],d2[,2])
+  test1 = cor.test(dat1[,1],dat1[,2])
+  test2 = cor.test(dat2[,1],dat2[,2])
+  
+  empr1 = round(test1$estimate,2)
+  empr2 = round(test2$estimate,2)
   
   z1 = round(psych::fisherz(test1$estimate), 2)
   z2 = round(psych::fisherz(test2$estimate), 2)
@@ -20,13 +23,17 @@ generate_corr_twogroup <- function(n1, r1, n2, r2, alpha=0.05) {
   
   zcrit = qnorm(1-alpha/2)
   
-  return(list(n1=n1,r1=r2,n2=n2,r2=r2,dat1,dat2,test1,test2,z1,z2,zdiff=zdiff, zcrit=zcrit))
+  return(list(n1=n1,r1=empr1,n2=n2,r2=empr2,dat1,dat2,test1,test2,z1,z2,zdiff=zdiff, zcrit=zcrit,z1=z1,z2=z2))
 }
 
-solution_corr_2grp <- function(x) {
-  l1 <- paste0("$$Z=\\frac{",x$z1,"-",x$z2,"}{\\sqrt{","\\frac{1}{",x$n1,"-3}+\\frac{1}{",x$n2,"-3}","}}$$\n")
-  l2 <- paste0("$$= ",x$zdiff,"$$\n")
-  
-  return(c(l1,l2))
+solution_corr_2grp <- function(x, round=2) {
+  l1 <- paste0("$$Z=\\frac{",x$z1,"-",x$z2,"}{\\sqrt{","\\frac{1}{",x$n1,"-3}+\\frac{1}{",x$n2,"-3}","}} = ",round(x$zdiff,round),"$$\n")
+  # l2 <- paste0("$$= ",round(x$zdiff,round),"$$\n")
+  return(l1)
+  # return(c(l1,l2))
 }
-#cat("Einsetzen in Formel 2.4 für den Test der Unterschiedlichkeit von Korrelationen in zwei unabhängigen Stichproben:\n")
+
+fisher_z_solution <- function(r, round=2) {
+  z = 0.5*log((1+r)/(1-r))
+  cat("$$ \\frac{1}{2} \\log \\frac{1+",r,"}{1-",r,"} = ",round(z,round)," $$ ")
+}
